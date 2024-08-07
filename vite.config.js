3fd78d4079a5c6d0fc51aa5c3a249b6935963bff
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-08-07 10:49:30 -0300

    menu hamburguesa arreglado

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        port: 9999, // Cambia '3000' al número de puerto que prefieras
+      },
   plugins: [react()],
 })
+
+
+
