49112115bea3e5f109abbb92e2b075d3f793b563
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-08-23 18:07:45 -0300

    grid de producto fncionando

--- src/components/products/Item.jsx
+++ src/components/products/Item.jsx
@@ -3,13 +3,12 @@ import React from 'react'
 const Item = ( {producto} ) => {
     //Un item es donde tenemos recien la visual a nuestro gusto de lo que recibimos, luego veremos como hacer para toquetear un poquito. aca luego tenemos que agregar el botoncito de compra etc. 
     return (
-        <div key={producto.id} className='product'>
-            <img src={producto.imagen} alt={producto.nombre} />
-            <h2>{producto.nombre}</h2>
-            <h4>${producto.precio}</h4>
-            <p>{producto.descripcion}</p>
-            <strong>{producto.categoria}</strong>
-            <p>Cantidad: {producto.stock}</p>
+        <div key={producto.id} className='product-card'>
+            <img className="product-image" src={producto.imagen} alt={producto.nombre} />
+            <h3 className="product-name" >{producto.nombre}</h3>
+            <h4>{producto.categoria}</h4>
+            <strong className="product-price" >${producto.precio}</strong>
+            <button className="add-to-cart-button">Agregar al carrito</button>
         </div>
     )
 }
--- src/components/products/ItemList.jsx
+++ src/components/products/ItemList.jsx
@@ -6,13 +6,13 @@ const ItemList = ( {productos} ) => {
     //en itemList solamente hacemos control de lo que se muestra en la pantalla, si productos, si grid, y condicionamos el entorno de donde luego vamos a tener nuestro grid ITEM
 
   return (
-    <div className='productos-grid'>
+    <div className='item-list'>
         {
         productos.length > 0 ? 
         productos.map(producto => {
             return <Item producto={producto} />
         })
-        : <p>No hay productos</p>
+        : <p>Cargando...</p>
         }
     </div>
   )
