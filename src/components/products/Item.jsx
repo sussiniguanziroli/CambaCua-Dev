a99c7c2040d0f53b17cd0c8f2f66ac58c2043c9f
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-08-29 19:10:12 -0300

    commit del chiche

--- src/components/products/Item.jsx
+++ src/components/products/Item.jsx
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import Flickity from 'flickity';
+import 'flickity/css/flickity.css'; // Importa los estilos de Flickity
 import { useCarrito } from '../../context/CarritoContext'; // Ajusta la ruta según tu estructura de carpetas
 
 
 const Item = ({ producto, notify }) => {
     const { agregarAlCarrito } = useCarrito();
     const [cantidad, setCantidad] = useState(1);
+    const flickityRef = useRef(null);
+
+
+    useEffect(() => {
+        if (flickityRef.current) {
+            new Flickity(flickityRef.current, {
+                cellAlign: 'left',
+                contain: true,
+                pageDots: false,  // Si no deseas que se muestren los puntos de navegación
+                prevNextButtons: true // Para mostrar las flechas de navegación
+            });
+        }
+    }, []);
+
+
 
     const handleAddToCart = () => {
         agregarAlCarrito(producto, cantidad);
@@ -12,7 +29,24 @@ const Item = ({ producto, notify }) => {
 
     return (
         <div key={producto.id} className='product-card'>
-            <img className="product-image" src={producto.imagen} alt={producto.nombre} />
+            <div className="carousel" ref={flickityRef}>
+                {producto.imagen && (
+                    <div className="carousel-cell">
+                        <img className="product-image" src={producto.imagen} alt={`${producto.nombre} - Imagen 1`} />
+                    </div>
+                )}
+                {producto.imagenB && (
+                    <div className="carousel-cell">
+                        <img className="product-image" src={producto.imagenB} alt={`${producto.nombre} - Imagen 2`} />
+                    </div>
+                )}
+                {producto.imagenC && (
+                    <div className="carousel-cell">
+                        <img className="product-image" src={producto.imagenC} alt={`${producto.nombre} - Imagen 3`} />
+                    </div>
+                )}
+            </div>
+
             <h3 className="product-name">{producto.nombre}</h3>
             <p className='product-category'>{producto.categoria}</p>
             <strong className="product-price">${producto.precio}</strong>
@@ -26,7 +60,7 @@ const Item = ({ producto, notify }) => {
                 />
                 <button onClick={() => setCantidad(cantidad + 1)}>+</button>
             </div>
-            <button className="add-to-cart-button" onClick={() => { handleAddToCart(); notify();}}>Agregar al carrito</button>
+            <button className="add-to-cart-button" onClick={() => { handleAddToCart(); notify(); }}>Agregar al carrito</button>
         </div>
     );
 };
