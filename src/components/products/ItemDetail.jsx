0c3c1154a5ff438855adbc0e821fd31be219dedf
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-09-20 18:04:17 -0300

    logica de detail

--- src/components/products/ItemDetail.jsx
+++ src/components/products/ItemDetail.jsx
@@ -1,29 +1,60 @@
 import React, { useContext } from 'react'
-import { NavLink } from 'react-router-dom'
+import { useCarrito } from '../../context/CarritoContext'
+import Swal from 'sweetalert2'
 
+const ItemDetail = ({ product, notifyDetailAgregado }) => {
 
-const ItemDetail = ({ product }) => {
+    const { agregarAlCarrito, carrito } = useCarrito();
 
-
-    
     //se puede acceder a carrito desde aca sin problema ademas de que va a hacer falta.
 
     //tambien tenemos que hacer boton para vover como teniamos en spirit y hacerlo responsive como no jaja
 
+    let cartItems = carrito;
+    const existsInCart = cartItems.some(item => item.id === product.id);
 
+
+    const handleAddToCart = () => {
+
+        if (existsInCart) {
+            // Mostrar alerta usando Toastify
+            Swal.fire({
+                title: "Producto ya en carrito",
+                text: "Para agregar mas de uno, debes seleccionar la cantidad desde el carrito!",
+                icon: "info",
+                confirmButtonColor: '#0b369c',
+            });
+        } else {
+            // Lógica para agregar el producto al carrito
+            agregarAlCarrito(product);
+            notifyDetailAgregado();
+        }
+
+
+
+    };
 
     return (
         <div className='detail'>
-            <div className='back-button nav-link'>
-                
-            </div>
-            <section className='detail-section'>
+            <div className='detail-img'>
                 <img src={product.imagen} alt={product.image} />
+            </div>
+            <div className='detail-text'>
                 <h1>{product.nombre}</h1>
+                <p>{product.descripcion}</p>
                 <strong>${product.precio}</strong>
-                <p>{product.description}</p>
-                
-            </section>
+                <p>Disponibilidad: {product.stock}</p>
+                <div>
+                    {product.stock === 0 ? (
+                        <div className="stock-status">Sin Stock</div>
+                    ) : (
+                        <button className="add-to-cart-button-detail" onClick={handleAddToCart}>
+                            {existsInCart ? "En carrito" : "Agregar al carrito"}
+                        </button>
+                    )}
+                </div>
+            </div>
+
         </div>
     )
 }
--- src/components/products/ItemDetailContainer.jsx
+++ src/components/products/ItemDetailContainer.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import ItemDetail from './ItemDetail';
-import {doc, getDoc } from "firebase/firestore";
-import {db} from '../../firebase/config'
-
+import { doc, getDoc } from "firebase/firestore";
+import { db } from '../../firebase/config'
+import { GridLoader } from 'react-spinners';
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { NavLink } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { FaMinus, FaPlus, FaShoppingCart, FaTimes, FaTrashAlt } from 'react-icons/fa';
 
 const ItemDetailContainer = () => {
 
@@ -15,17 +20,32 @@ const ItemDetailContainer = () => {
         const documentRef = doc(db, "productos", itemId);
         getDoc(documentRef)
             .then(res => {
-                setProducto({...res.data(), id: res.id});
+                setProducto({ ...res.data(), id: res.id });
             });
 
     }, [itemId])
 
+    const notifyDetailAgregado = () => toast.success("Producto agregado al carrito!");
 
-  return (
-    <div className='item-detail-loading'>{producto ? 
-        <ItemDetail product={producto}/>
-        : <div className='gif-carga-item'></div> }</div>
-  )
+    
+
+
+    return (
+        <main className='item-detail-loading'>
+            
+            <ToastContainer
+                autoClose={1500}
+
+                position="top-left"
+            />
+            <NavLink className="back-button" to="/productos"><IoMdArrowRoundBack /></NavLink>
+            {producto ?
+                <ItemDetail notifyDetailAgregado={notifyDetailAgregado} product={producto} />
+                : <div className='loader'>
+                    <GridLoader color="#0b369c" size={25} margin={2} />
+                </div>}
+        </main>
+    )
 }
 
 export default ItemDetailContainer
