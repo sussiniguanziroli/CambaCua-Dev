9612be5af5f23d02fb36abdec9244d0ce9d12d57
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-10-21 11:40:07 -0300

    tratemos de footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+
 
 const Footer = () => {
-  return (
-    <footer className="landing-footer">
-        <div className="footer-links">
-          <a href="#">2024 Todos los derechos reservados</a>
-          <a href="#">CambaCuaVet</a>
-        </div>
-        <div className="footer-contact">
-        <NavLink className="nav-button" activeclassname="active" to="/contacto" >Contacto</NavLink>  
-          <div className="social-icons">
-            
-          </div>
-        </div>
-      </footer>
-  )
-}
+    return (
+        <footer className="footer">
+            <div className="footer-container">
+                <div className="footer-section">
+                    <h4>Cambá Cuá Vet Shop</h4>
+                    <p>Tu tienda de confianza para el cuidado de tus mascotas.</p>
+                </div>
+                <div className="footer-section">
+                    <h4>Contacto</h4>
+                    <ul>
+                        <li><a href="tel:+5493704000000">+54 9 3704 000000</a></li>
+                        <li><a href="mailto:info@cambacuavetshop.com">info@cambacuavetshop.com</a></li>
+                    </ul>
+                </div>
+                <div className="footer-section">
+                    <h4>Síguenos</h4>
+                    <ul className="social-media-links">
+                        <li><a href="#"><i className="fab fa-facebook"></i></a></li>
+                        <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+                        <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="footer-bottom">
+                <p>&copy; {new Date().getFullYear()} Cambá Cuá Vet Shop - Todos los derechos reservados.</p>
+            </div>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
